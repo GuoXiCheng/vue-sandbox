70059de9ee3b9316afbe12bba139195034e1cafb
Xicheng Guo <xicheng_guo@wistron.com>
2024-07-16 16:46:25 +0800

    add Attribute Bind

---
+++ src/components/TemplateSyntax/AttributeBindMultipleValued.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="div-box">
+        <p v-bind="objectOfAttrs">Attribute Bind Multiple Valued</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+const objectOfAttrs = {
+    id: 'id1',
+    class: 'class1',
+    style: 'color: red'
+}
+</script>
+
+<style scoped>
+.class1 {
+    font-size: 20px;
+    font-weight: bold;
+}
+</style>
--- src/views/TemplateSyntaxView.vue
+++ src/views/TemplateSyntaxView.vue
@@ -1,7 +1,13 @@
 <template>
     <Interpolation />
+    <AttributeBind />
+    <AttributeBindBool />
+    <AttributeBindMultipleValued />
 </template>
 
 <script setup>
 import Interpolation from '@/components/TemplateSyntax/Interpolation.vue';
+import AttributeBind from '@/components/TemplateSyntax/AttributeBind.vue';
+import AttributeBindBool from '@/components/TemplateSyntax/AttributeBindBool.vue';
+import AttributeBindMultipleValued from '@/components/TemplateSyntax/AttributeBindMultipleValued.vue';
 </script>
