73039c88793fd6da85503d0b31ac980fe39e7747
Xicheng Guo <xicheng_guo@wistron.com>
2024-10-10 09:49:05 +0800

    update

---
+++ src/components/ComputedAndWatch/WatchEffect.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="div-box">
+    <button @click="todoId++">Fetch Todo</button>
+    <p v-if="data">{{ data }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { watchEffect, ref } from 'vue'
+const todoId = ref(1)
+const data = ref(null)
+
+// #region snippet
+watchEffect(async () => {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId.value}`)
+  data.value = await response.json()
+})
+// #endregion snippet
+</script>
--- src/views/ComputedAndWatchView.vue
+++ src/views/ComputedAndWatchView.vue
@@ -6,6 +6,8 @@
   <WatchGetter />
   <WatchArr />
   <WatchDeep />
+  <WatchEffectWatch />
+  <WatchEffect />
 </template>
 
 <script setup>
@@ -14,6 +16,8 @@ import ComputedWritable from '@/components/ComputedAndWatch/ComputedWritable.vue
 import WatchArr from '@/components/ComputedAndWatch/WatchArr.vue'
 import WatchBasic from '@/components/ComputedAndWatch/WatchBasic.vue'
 import WatchDeep from '@/components/ComputedAndWatch/WatchDeep.vue'
+import WatchEffectWatch from '@/components/ComputedAndWatch/WatchEffectWatch.vue'
+import WatchEffect from '@/components/ComputedAndWatch/WatchEffect.vue'
 import WatchGetter from '@/components/ComputedAndWatch/WatchGetter.vue'
 import WatchRef from '@/components/ComputedAndWatch/WatchRef.vue'
 </script>
